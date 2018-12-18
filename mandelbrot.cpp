238310d084b26d6720d2a925353a0999cd0a9e9e
Sarah <morningcrafter2003@gmail.com>
2018-12-18 21:42:40 +0100

    Changes

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -8,7 +8,7 @@
 #define WIDTH 800
 #define HEIGHT 600
 
-#define ITS 0
+#define ITS 10
 
 int c_x, c_y = 0; // Current x and y position
 
@@ -24,6 +24,15 @@ sf::Clock timeClock;
 sf::Time timeFinished;
 
 bool printed = false; // A flag for if the time was already printed.
+
+sf::Color colors[6] = {
+	sf::Color(0x9400d3), // violet
+	sf::Color(0x0000ff), // blue
+	sf::Color(0x00ff00), // green
+	sf::Color(0xffff00), // yellow
+	sf::Color(0xff7f00), // orange
+	sf::Color(0xff0000)  // red
+};
 
 // This function gets the mapped complex equivalent to an (x, y) coordinate pair.
 std::complex<double> getC(int x, int y) {
@@ -41,24 +50,24 @@ std::complex<double> m(std::complex<double> z, std::complex<double> c) {
 }
 
 // This function iterates the m(z, c) function up to ITS times and checks if the bound (in this case sqrt(2)) is reached.
-bool iter(std::complex<double> c) {
+int iter(std::complex<double> c) {
 	std::complex<double> res;
 	res.real(0.0); // Set the first z value to 0 + 0i.
 	res.imag(0.0);
 
 	for (int i = 0; i < ITS; i++) {
 		res = m(res, c);
-	}
 
-	if (std::abs(res) > sqrt(2))
-		return false;
+		if (std::abs(res) > sqrt(2))
+			return 1;
+	}
 	
-	return true;
+	return -1;
 }
 
 // Calculates the next coordinate. Simulates two nested for loops.
-bool nextStep(void) {
-	bool result;
+int nextStep(void) {
+	int result;
 
 	result = iter(getC(c_x, c_y));
 
@@ -66,6 +75,7 @@ bool nextStep(void) {
 	if (c_x == WIDTH) { // If c_x reached the limit
 		c_x = 0;
 		c_y++;
+		std::cout << "Row " << c_y - 1 << " done." << std::endl;
 
 		if (c_y == HEIGHT) { // If c_y reached the limit
 			c_x = WIDTH - 1; // To avoid drawing outside of the image.
@@ -83,9 +93,9 @@ int main(int argc, char **argv) {
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set Viewer");
 
 	sf::Image screen;
-	screen.create(WIDTH, HEIGHT, sf::Color::White);
+	screen.create(WIDTH, HEIGHT, sf::Color::Black);
 
-	timeClock.restart(); // Set the clock to 0, because the clock starts counting at creation.
+	/*timeClock.restart(); // Set the clock to 0, because the clock starts counting at creation.*/
 
 	while (window.isOpen()) {
 		sf::Event event;
@@ -96,8 +106,9 @@ int main(int argc, char **argv) {
 		}
 
 		if (!done) {
-			if (nextStep()) // Calculate if the next point is in the set and add it (or not).
-				screen.setPixel(c_x, c_y, sf::Color::Black);
+			int result = nextStep();
+			if (result >= 0) // Calculate if the next point is in the set and add it (or not).
+				screen.setPixel(c_x, c_y, sf::Color::White);
 
 		} else if (!printed) { // If everything's done, print the elapsed time.
 			std::cout << "Time elapsed when finished drawing: " << timeFinished.asSeconds() << "s." << std::endl;
